dd9e1d03f43aada218d53169b51b50e24c344743
Emanuel Guimarães <emanuelguimaraes23@gmail.com>
2016-10-25 09:20:45 -0300

    Refatoração da classe ExtractHTML

--- ProBet/src/br/edu/controller/ExtractHTML.java
+++ ProBet/src/br/edu/controller/ExtractHTML.java
@@ -1,8 +1,6 @@
 package br.edu.controller;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,53 +14,55 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import br.edu.model.Match;
+import br.edu.util.Constantes;
 
+@SuppressWarnings("deprecation")
 public class ExtractHTML {
 
-	public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	public static String extractHTML(String url) {
+		StringBuffer sb = new StringBuffer();
+		
 		try {
-			HttpClient httpclient = new DefaultHttpClient(); // Create HTTP
-																// Client
-			HttpGet httpget = new HttpGet("https://www.academiadasapostas.com/stats/team/alemanha/bayern-munique/961"); // Set
-																												// the
-			// action
-			// you want
-			// to do
-			HttpResponse response = httpclient.execute(httpget); // Executeit
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpget = new HttpGet(url);
+			HttpResponse response = httpclient.execute(httpget);
 			HttpEntity entity = response.getEntity();
 			InputStream is;
 			is = entity.getContent();
 			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
-			String line = null;
 			Match match;
 			List<Match> listMatchs = new ArrayList<Match>();
-			while ((line = reader.readLine()) != null) { // Read line by line
-				if (line.contains("<table class=\"next-games\"")) {
-					while (((line = reader.readLine()) != null) && (!line.trim().equals("</div>"))) {
-						if ((line.contains("<tr class=\"even\">")) || (line.contains("<tr class=\"odd\">"))) {
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is, Constantes.UTF_8), 8);
+			String line = null;
+			
+			while ((line = reader.readLine()) != null) {
+				if (line.contains(Constantes.NEXT_GAMES)) {
+					while (((line = reader.readLine()) != null) && (!line.trim().equals(Constantes.FECHA_DIV))) {
+						if ((line.contains(Constantes.EVEN)) || (line.contains(Constantes.ODD))) {
 							match = new Match();
-							while (((line = reader.readLine()) != null) && (!line.trim().equals("</tr>"))) {
-								if ((line.contains("<td>")) && (line.contains("</td>")) && (match.getDateMatch() == null)) {
-									line = line.replace("<td>", "").replace("</td>", "");
+							while (((line = reader.readLine()) != null) && (!line.trim().equals(Constantes.FECHA_TR))) {
+								if ((line.contains(Constantes.TD)) && (line.contains(Constantes.FECHA_TD)) && (match.getDateMatch() == null)) {
+									line = line.replace(Constantes.TD, Constantes.VAZIO).replace(Constantes.FECHA_TD, Constantes.VAZIO);
 									match.setDateMatch(line);
-								} else if ((line.contains("</ul>")) && (line.contains("</a>"))) {
-									line = line.replace("</ul>", "").replace("</a>", "");
-									match.setNameCompetition(line.replaceAll("\\s+$", "").replaceAll("^\\s+", ""));
-								} else if ((line.contains("a href=\"")) && (line.contains("target=\"_self\">")) && (match.getNameTeamA() == null)) {
+								} else if ((line.contains(Constantes.FECHA_UL)) && (line.contains(Constantes.FECHA_A))) {
+									line = line.replace(Constantes.FECHA_UL, Constantes.VAZIO).replace(Constantes.FECHA_A, Constantes.VAZIO);
+									match.setNameCompetition(line.replaceAll(Constantes.REGEX_ESPACO_INICIO, Constantes.VAZIO).replaceAll(Constantes.REGEX_ESPACO_FIM, Constantes.VAZIO));
+								} else if ((line.contains(Constantes.HREF)) && (line.contains(Constantes.TARGET_SELF)) && (match.getNameTeamA() == null)) {
 									line = reader.readLine();
-									if ((line.contains("</a>")) && (!line.contains("<ul class="))) {
-										match.setNameTeamA(line.replace("</a>", "").replaceAll("\\s+$", "").replaceAll("^\\s+", ""));
+									if ((line.contains(Constantes.FECHA_A)) && (!line.contains(Constantes.UL_CLASS))) {
+										match.setNameTeamA(line.replace(Constantes.FECHA_A, Constantes.VAZIO).replaceAll(Constantes.REGEX_ESPACO_INICIO, Constantes.VAZIO).replaceAll(Constantes.REGEX_ESPACO_FIM, Constantes.VAZIO));
 									}
-								} else if (line.contains("</a>") && (line.contains("-")) && (!line.contains("target=\"_self")) && (!line.contains("<a href=\"")) && (!line.contains("<a class=\"match_preview\" href="))) {
-									line = line.replace("</a>", "");
-									String[] resultSplit = line.trim().split("-");
+								} else if (line.contains(Constantes.FECHA_A) && (line.contains(Constantes.HIFEN)) && (!line.contains(Constantes.TARGET_SELF)) && (!line.contains(Constantes.HREF)) && (!line.contains(Constantes.MATCH_PREVIEW))) {
+									line = line.replace(Constantes.FECHA_A, Constantes.VAZIO);
+									String[] resultSplit = line.trim().split(Constantes.HIFEN);
 									match.setScoreTeamA(Integer.parseInt((resultSplit[0]).trim()));
 									match.setScoreTeamB(Integer.parseInt((resultSplit[1]).trim()));
-								} else if ((line.contains("a href=\"")) && (line.contains("target=\"_self\">"))) {
+								} else if ((line.contains(Constantes.HREF)) && (line.contains(Constantes.TARGET_SELF))) {
 									line = reader.readLine();
-									if ((line.contains("</a>")) && (!line.contains("<ul class="))) {
-										match.setNameTeamB(line.replace("</a>", "").replaceAll("\\s+$", "").replaceAll("^\\s+", ""));
+									if ((line.contains(Constantes.FECHA_A)) && (!line.contains(Constantes.UL_CLASS))) {
+										match.setNameTeamB(line.replace(Constantes.FECHA_A, Constantes.VAZIO).replaceAll(Constantes.REGEX_ESPACO_INICIO, Constantes.VAZIO).replaceAll(Constantes.REGEX_ESPACO_FIM, Constantes.VAZIO));
 									}
 								}
 							}
@@ -75,20 +75,20 @@ public class ExtractHTML {
 				}
 			}
 
-			BufferedWriter buffWrite = new BufferedWriter(new FileWriter("C:/Users/Emanuel Guimarães/Desktop/AngularJS/TESTE_EMANUEL/SAIDA.txt"));
 			for (Match aux : listMatchs) {
-				buffWrite.append(aux.toString());
-				buffWrite.newLine();
+				sb.append(aux.toString());
+				sb.append(Constantes.PULA_LINHA);
 			}
 
-			is.close(); // Close the stream
+			is.close(); 
 			reader.close();
-			buffWrite.close();
 		} catch (UnsupportedOperationException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		return sb.toString();
 	}
 
 	public static String getDateLine(String line) {
