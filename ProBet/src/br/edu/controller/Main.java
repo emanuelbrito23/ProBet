c08f46ed8358df52f9eb3029709c0a462b5ed206
Emanuel Guimarães <emanuelguimaraes23@gmail.com>
2016-10-25 09:22:40 -0300

    Commit classes Constantes e Main

---
+++ ProBet/src/br/edu/controller/Main.java
@@ -0,0 +1,8 @@
+package br.edu.controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(ExtractHTML.extractHTML("https://www.academiadasapostas.com/stats/team/inglaterra/liverpool/663"));
+	}
+}
---
+++ ProBet/src/br/edu/util/Constantes.java
@@ -0,0 +1,26 @@
+package br.edu.util;
+
+public class Constantes {
+	
+	public final static String NEXT_GAMES = "<table class=\"next-games\"";
+	public final static String FECHA_DIV = "</div>";
+	public final static String EVEN = "<tr class=\"even\">";
+	public final static String ODD = "<tr class=\"odd\">";
+	public final static String FECHA_TR = "</tr>";
+	public final static String TD = "<td>";
+	public final static String FECHA_TD = "</td>";
+	public final static String VAZIO = "";
+	public final static String FECHA_UL = "</ul>";
+	public final static String FECHA_A = "</a>";
+	public final static String REGEX_ESPACO_INICIO = "\\s+$";
+	public final static String REGEX_ESPACO_FIM = "^\\s+";
+	public final static String TARGET_SELF = "target=\"_self\">";
+	public final static String HREF = "a href=\"";
+	public final static String UL_CLASS = "<ul class=";
+	public final static String HIFEN = "-";
+	public final static String MATCH_PREVIEW = "<a class=\"match_preview\" href=";
+	public final static String UTF_8 = "UTF-8";
+	public final static String PULA_LINHA = "\n";
+	
+
+}
